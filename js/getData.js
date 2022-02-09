94955805d9b084a303f6ed603bc3568285e53e2c
Daria Ivanova <daria.ivanova13@yandex.ru>
2022-02-09 13:50:15 +0300

    add getData function

---
+++ js/getData.js
@@ -0,0 +1,20 @@
+// Напишите `GET` запрос получения данных с сервера `/data`
+// Выведите массив с данными в консоль
+// Добавьте функции параметр callback
+// После получения данных, если callback был передан вызовите его с полученными данными
+
+
+export const getData = (callback) => {
+    fetch(new URL('data', window.location.href)) //window.location.href = http://localhost:3000, =>new URL = http://localhost:3000/data
+    .then((response) =>{
+        //console.log(response.json);
+       return response.json();      
+    })
+    .then((data) => {
+        console.log(data);
+        if (typeof callback === 'function') {
+            callback(data);
+        }
+    })
+    .catch(console.error);
+};
